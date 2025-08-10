6baa5ceedd91af67d049eb4f71f13a082e3c92ce
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:26:42 +0100

    updating: page

---
+++ app/dashboard/admin/video-processing/page.tsx
@@ -0,0 +1,9 @@
+import { VideoProcessingPipeline } from "@/components/video/video-processing-pipeline"
+
+export default function VideoProcessingPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <VideoProcessingPipeline />
+    </div>
+  )
+}
