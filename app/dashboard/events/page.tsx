11aa236b40a0d49d074a4288feae222176782ee8
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:27:30 +0100

    updating: page

---
+++ app/dashboard/events/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import EventSchedulingSystem from "@/components/events/event-scheduling-system"
+
+export default function EventsPage() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <div className="max-w-6xl mx-auto">
+        <EventSchedulingSystem />
+      </div>
+    </div>
+  )
+}
