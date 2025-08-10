a7ed1944236a174850a9ef6757abeb261e24d0f2
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:26:15 +0100

    updating: page

---
+++ app/dashboard/admin/moderation/page.tsx
@@ -0,0 +1,9 @@
+import { ContentModerationSystem } from "@/components/moderation/content-moderation-system"
+
+export default function ContentModerationPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <ContentModerationSystem />
+    </div>
+  )
+}
