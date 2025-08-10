2802799d22d17856325344563269046008c05967
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:26:47 +0100

    updating: page

---
+++ app/dashboard/analytics/advanced/page.tsx
@@ -0,0 +1,9 @@
+import { AdvancedAnalyticsDashboard } from "@/components/analytics/advanced-analytics-dashboard"
+
+export default function AdvancedAnalyticsPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <AdvancedAnalyticsDashboard />
+    </div>
+  )
+}
