e52e0a0634af45cfb1df31e71c4654cb97a5534d
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:27:55 +0100

    updating: page

---
+++ app/dashboard/i18n/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import MultiLanguageSystem from "@/components/i18n/multi-language-system"
+
+export default function MultiLanguagePage() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <div className="max-w-6xl mx-auto">
+        <MultiLanguageSystem />
+      </div>
+    </div>
+  )
+}
