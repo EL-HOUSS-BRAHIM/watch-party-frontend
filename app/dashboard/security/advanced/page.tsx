90637dbfe32e60df6c7862fa4a22e5681bcc6275
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:29:07 +0100

    updating: page

---
+++ app/dashboard/security/advanced/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import AdvancedSecuritySystem from "@/components/security/advanced-security-system"
+
+export default function AdvancedSecurityPage() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <div className="max-w-6xl mx-auto">
+        <AdvancedSecuritySystem />
+      </div>
+    </div>
+  )
+}
