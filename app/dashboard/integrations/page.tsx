7f5eb90cca298b35ad4d20e1fad523eb2b07dc39
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-08-10 20:27:58 +0100

    updating: page

---
+++ app/dashboard/integrations/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import IntegrationAPISystem from "@/components/integrations/integration-api-system"
+
+export default function IntegrationsPage() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <div className="max-w-6xl mx-auto">
+        <IntegrationAPISystem />
+      </div>
+    </div>
+  )
+}
